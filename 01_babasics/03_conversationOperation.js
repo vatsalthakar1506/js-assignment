f77d7ec5a87597755ac176e935e4e372f1a0dcf8
vatsalthakar1506 <114866170+vatsalthakar1506@users.noreply.github.com>
2024-07-09 08:00:26 +0000

    conversion of data types

---
+++ 01_babasics/03_conversationOperation.js
@@ -0,0 +1,15 @@
+let score = null
+
+
+// console.log(typeof score);
+
+let valueNumber  = Number(score)
+
+// console.log(valueNumber);
+
+
+let  isLoggedIn = "asdasd"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn);
