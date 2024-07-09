17a98206ecf220771e3e0c8da57896ba19801321
vatsalthakar1506 <114866170+vatsalthakar1506@users.noreply.github.com>
2024-07-09 07:37:34 +0000

    data types

---
+++ 01_babasics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail =  "vatz@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+
+// accountId = 2 //not Allowed 
+
+accountEmail = "hc@gmail.com"
+accountPassword = "2121212121"
+accountCity = "Bengluru"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
