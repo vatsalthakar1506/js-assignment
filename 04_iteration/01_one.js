c5cb1c68645c06119b890357b8b55c79489f2b10
vatsalthakar1506 <114866170+vatsalthakar1506@users.noreply.github.com>
2024-07-12 15:09:38 +0000

    js concepts

---
+++ 04_iteration/01_one.js
@@ -0,0 +1,68 @@
+
+
+
+// for (let i=0; i <=10;  i++){
+  
+//     const element = i
+//     console.log(i);
+// }
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+
+// while (arr < myArray.length){
+
+//     console.log(`Vaue is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 2
+
+// do{
+//     console.log(`Score is ${score}`);
+//     score++
+// }while(score <=10)
+
+
+
+
+// const myObj = {
+//     game1 : "NFS",
+//     game2 : "Spiderman", 
+    
+// }
+
+
+// for(let val of myObj){
+//     console.log("Values is --",val);
+// }
+
+
+// const myObject = {
+//     js : "Javascript",
+//     cpp : "C++",
+//     rb : "Ruby",
+//     swift : "Swift by apple"
+// }
+
+// for (const key in myObject){
+
+//     console.log(myObject[key]);
+
+// }
+
+
+
+
+const map = new Map()
+map.set("IN", "India")
+map.set("USA", "United States of America")
+map.set("Fr", "France")
+
+// console.log(map)
+
+for (let key in map){
+    
+    console.log("Key ---->",key)
+}
---
+++ 04_iteration/foreach.js
@@ -0,0 +1,35 @@
+const coding = ["Js", "Ruby", "Python", "Cpp"]
+
+// coding.forEach(function (val) {
+
+//     console.log(val);
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item) => {
+//     console.log("YITEM ", item);
+// })
+
+// coding.forEach((item, index, arr)=>{
+
+//     console.log(item,index);
+// })
+
+const myCoding = ["JS", "ruby", "java", "python", "cpp"]
+
+const values = myCoding.forEach((item)=>{
+        // console.log(item);
+        return item
+})
+
+console.log(values);
